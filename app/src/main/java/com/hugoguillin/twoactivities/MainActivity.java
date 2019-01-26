958e67cd90e72b54fab2ff86ff79a50afd1854a1
Hugo Guillín <hugoguillin@hotmail.com>
2019-01-26 18:24:40 +0100

    Task2_Tarea2_2

--- app/src/main/java/com/hugoguillin/twoactivities/MainActivity.java
+++ app/src/main/java/com/hugoguillin/twoactivities/MainActivity.java
@@ -2,6 +2,7 @@ package com.hugoguillin.twoactivities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -28,6 +29,15 @@ public class MainActivity extends AppCompatActivity {
         mMessageEditText=findViewById(R.id.editText_main);
         mReplyHeadTextView = findViewById(R.id.text_header_reply);
         mReplyTextView = findViewById(R.id.text_message_reply);
+
+        if(savedInstanceState != null){
+            boolean isVisible = savedInstanceState.getBoolean("reply_visible");
+            if(isVisible){
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(savedInstanceState.getString("reply_text"));
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
     }
 
     @Override
@@ -64,6 +74,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.d(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if(mReplyHeadTextView.getVisibility() == View.VISIBLE){
+            outState.putBoolean("reply_visible", true);
+            outState.putString("reply_text", mReplyTextView.getText().toString());
+        }
     }
 
     public void launchSecondActivity(View view) {
